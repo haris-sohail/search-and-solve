eda412e50d2303c3792e8ea13a4bda5d74c3ca7f
Haris Sohail <harissohail.cs@gmail.com>
2024-03-01 19:18:23 +0500

    Calculate costs of all permutations

--- util.py
+++ util.py
@@ -62,24 +62,98 @@ def getVertices(fileLines):
 def getVerticesNames(vertices):
     verticesNames = []
     
-    for i in range(0, len(vertices)):
+    for i in range(1, len(vertices) + 1):
         verticesNames.append(i)
         
     return verticesNames
 
+def getVerticesBefore(vertexToCheck, perm):
+    verticesBefore = []
+    for vertex in perm:
+        if vertex != int(vertexToCheck.name):
+             verticesBefore.append(str(vertex))
+        else:
+            break
+        
+    return verticesBefore
 
+def appendExactlyEqualParentSets(parentSets, verticesBefore, vertex):
+    for key in vertex.info:
+        if vertex.info[key][0] == verticesBefore:
+            parentSets.append(vertex.info[key])
+            
+def appendOneMatchingParentSet(parentSets, vertexToMatch, vertex):
+    for key in vertex.info:
+        if vertex.info[key][0] == vertexToMatch:
+            parentSets.append(vertex.info[key])
+
+def getConsistentParentSets(perm, vertexIndex, startIndex):
+    vertex = vertices[vertexIndex]
+    parentSets = []
+    
+    parentSets.append(vertex.info[1])
+    
+    if startIndex: # first vertex in the permutation
+        return parentSets
+        
+    else:
+        verticesBefore = getVerticesBefore(vertex, perm)
+        
+    # for the vertices before, check matching parent sets
+    
+    appendExactlyEqualParentSets(parentSets, verticesBefore, vertex)
+    
+    if len(verticesBefore) != 1:
+        for vertexToMatch in verticesBefore:
+            appendOneMatchingParentSet(parentSets, list(vertexToMatch), vertex)
+        
+    return parentSets
+
+def getCostOfPerm(perm): # e.g. 0 1 2 3 4
+    
+    cost = 0
+    i = 0
+    
+    # for every vertex get its consistent parent set
+    for vertex in perm:
+        min = float('inf')
+        consistentParentSets = getConsistentParentSets(perm, vertex - 1, i == 0)
+        i += 1
+        
+        # get min-cost parent set 
+        for parentSet in consistentParentSets:
+            if float(parentSet[1]) < min:
+                min = float(parentSet[1])
+                
+        cost += min
+        
+    return cost
+        
+
+def getCostAllPerm(allPerm):
+    costs = []
+    
+    for perm in allPerm:
+        costs.append(getCostOfPerm(perm))
+        
+    return costs
+    
 def execute(filename):
     fileLines = readFileIntoList(filename)
 
+    global vertices
     vertices = getVertices(fileLines) 
     
     verticesNames = getVerticesNames(vertices)
     
     # get all permutations
     
-    allPerm = list(itertools.permutations(verticesNames))
+    allPerm = list(itertools.permutations(verticesNames))    
     
+    # get cost of all permutations
     
+    allPermCosts = getCostAllPerm(allPerm)
+
     
     
     
